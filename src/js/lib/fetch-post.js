6bff5b9533f5e3a137c0b628f6141ae076912235
João Jerónimo <jj@crowdprocess.com>
2013-08-25 16:50:22 +0100

    load posts from localStorage

--- src/js/lib/fetch-post.js
+++ src/js/lib/fetch-post.js
@@ -6,6 +6,11 @@ var baseUrl = '/posts/';
 exports =
 module.exports =
 function fetchPost (post, callback) {
+  if (localStorage[post.title]) {
+    var storedPost = JSON.parse(localStorage[post.title]);
+    return callback(null, storedPost.body);
+  }
+
   hyperquest.get(baseUrl+post.file, gotPost);
 
   function gotPost(err, res) {
